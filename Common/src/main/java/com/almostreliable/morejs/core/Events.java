41280e6919f049465d771ff19b43e43abd7fe71f
LLytho <29131229+LLytho@users.noreply.github.com>
2022-08-30 12:24:20 +0200

    Rename events

--- Common/src/main/java/com/almostreliable/morejs/core/Events.java
+++ Common/src/main/java/com/almostreliable/morejs/core/Events.java
@@ -1,13 +1,13 @@
 package com.almostreliable.morejs.core;
 
 public class Events {
-    public static final String VILLAGER_TRADING = "morejs.villager.trading";
-    public static final String WANDERING_TRADING = "morejs.wanderer.trading";
+    public static final String VILLAGER_TRADING = "morejs.villager.trades";
+    public static final String WANDERING_TRADING = "morejs.wanderer.trades";
+    public static final String PLAYER_START_TRADING = "morejs.player.start_trading";
     public static final String ENCHANTMENT_TABLE_CHANGED = "morejs.enchantment_table.changed";
     public static final String ENCHANTMENT_TABLE_ENCHANT = "morejs.enchantment_table.enchant";
     public static final String ENCHANTMENT_TABLE_TOOLTIP = "morejs.enchantment_table.tooltip";
     public static final String TELEPORT = "morejs.teleport";
     public static final String STRUCTURE_LOAD = "morejs.structure.load";
     public static final String XP_CHANGE = "morejs.player.xp_change";
-    public static final String PLAYER_START_TRADING = "morejs.player.start_trading";
 }
