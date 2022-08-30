9cfa3a2a6d3db8b39fd2cc59c91def451b7424ef
LLytho <29131229+LLytho@users.noreply.github.com>
2022-08-30 14:34:05 +0200

    Add `getRemainingExperience` to xp change event

--- Common/src/main/java/com/almostreliable/morejs/features/misc/ExperiencePlayerEventJS.java
+++ Common/src/main/java/com/almostreliable/morejs/features/misc/ExperiencePlayerEventJS.java
@@ -56,6 +56,10 @@ public class ExperiencePlayerEventJS extends PlayerEventJS {
         return player.getXpNeededForNextLevel();
     }
 
+    public int getRemainingExperience() {
+        return (int) (getXpNeededForNextLevel() - (getExperienceProgress() * getXpNeededForNextLevel()));
+    }
+
     public boolean willLevelUp() {
         return getExperienceProgress() + (getAmount() / (float) getXpNeededForNextLevel()) >= 1.0F;
     }
