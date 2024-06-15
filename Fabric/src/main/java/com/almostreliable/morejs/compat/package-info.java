181728ec92fc0fcde82fb1b671f1e440af3b5383
LLytho <main@lytho.dev>
2024-06-15 23:10:39 +0200

    Remove fabric stuff

--- Fabric/src/main/java/com/almostreliable/morejs/compat/package-info.java
+++
@@ -1,6 +0,0 @@
-@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
-package com.almostreliable.morejs.compat;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
